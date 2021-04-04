6e3588c82b5d95d22a922149b6cb0852b11003d3
mahidul-islam <mizihan84@gmail.com>
2021-04-04 18:17:17 +0600

    added paddle and bounce in pong

--- src/Main.elm
+++ src/Main.elm
@@ -8,7 +8,17 @@ import Svg.Attributes exposing (cx, cy, height, r, viewBox, width, x, y)
 
 
 type alias Model =
-    Ball
+    { ball : Ball
+    , paddle : Paddle
+    }
+
+
+type alias Paddle =
+    { x : Int
+    , y : Int
+    , width : Int
+    , height : Int
+    }
 
 
 type alias Ball =
@@ -19,13 +29,29 @@ type alias Ball =
     }
 
 
-ball =
-    { x = 250, y = 250, radious = 10, speedX = 4 }
+initBall : Ball
+initBall =
+    { x = 250
+    , y = 260
+    , radious = 10
+    , speedX = 4
+    }
+
+
+initPaddle : Paddle
+initPaddle =
+    { x = 480
+    , y = 225
+    , width = 10
+    , height = 50
+    }
 
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( ball
+    ( { ball = initBall
+      , paddle = initPaddle
+      }
     , Cmd.none
     )
 
@@ -47,7 +73,31 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         OnAnimationFrame timeDelta ->
-            ( { model | x = model.x + model.speedX }, Cmd.none )
+            let
+                ball =
+                    model.ball
+
+                shouldBounce =
+                    shouldBallBounce model.paddle model.ball
+
+                speedX =
+                    if shouldBounce then
+                        ball.speedX * -1
+
+                    else
+                        ball.speedX
+
+                updatedBall =
+                    { ball | x = ball.x + speedX, speedX = speedX }
+            in
+            ( { model | ball = updatedBall }, Cmd.none )
+
+
+shouldBallBounce : Paddle -> Ball -> Bool
+shouldBallBounce paddle ball =
+    (ball.x + ball.radious >= paddle.x)
+        && (ball.y >= paddle.y)
+        && (ball.y <= paddle.y + 50)
 
 
 main : Program Flags Model Msg
@@ -60,30 +110,35 @@ main =
         }
 
 
-view : Ball -> Svg.Svg Msg
-view model =
+view : Model -> Svg.Svg Msg
+view { ball, paddle } =
     svg
         [ width "500"
         , height "500"
         , viewBox "0 0 500 500"
         , Svg.Attributes.style "background: #efefef"
         ]
-        [ viewBall model
-        , rect
-            [ x "480"
-            , y "225"
-            , width "10"
-            , height "50"
-            ]
-            []
+        [ viewBall ball
+        , viewPaddle paddle
         ]
 
 
+viewPaddle : Paddle -> Svg.Svg Msg
+viewPaddle paddle =
+    rect
+        [ x <| String.fromInt paddle.x
+        , y <| String.fromInt paddle.y
+        , width <| String.fromInt paddle.width
+        , height <| String.fromInt paddle.height
+        ]
+        []
+
+
 viewBall : Ball -> Svg.Svg Msg
-viewBall { x, y, radious } =
+viewBall ball =
     circle
-        [ cx <| String.fromInt x
-        , cy <| String.fromInt y
-        , r <| String.fromInt radious
+        [ cx <| String.fromInt ball.x
+        , cy <| String.fromInt ball.y
+        , r <| String.fromInt ball.radious
         ]
         []
